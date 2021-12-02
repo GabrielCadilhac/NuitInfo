5e0c105042cbce2279c26b692711f2bb9d90d78d
napkel <78849832+napkel@users.noreply.github.com>
2021-12-02 19:57:43 +0100

    base fichier java

---
+++ GestionFichier_Java/src/Fichier.java
@@ -0,0 +1,14 @@
+import java.awt.Image;
+import java.util.Date;
+
+public class Fichier {
+	public String nomF;
+	public Date dateLOpened;
+	public Image icone;
+	public boolean isFav;
+	public int note;
+	public String type;
+	
+	
+	
+}
