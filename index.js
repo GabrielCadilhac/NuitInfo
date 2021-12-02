ea9e8ce7c6b9167ad12ec79634a5c95a82ceccbd
LePaludV <59608384+LePaludV@users.noreply.github.com>
2021-12-02 18:43:35 +0100

    gitignore

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const http = require("http");
 const express = require("express");
 
+const pug=require('pug');
+
 var app = express();
 var server = http.createServer(app);
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
+app.set('view engine', 'pug');
+app.set('views', 'views/');
 
-app.use("public", express.static("public"));
+
+
+
 
 server.listen(process.env.PORT || 80, function()
 {
         console.log("Serveur démarré");
+        app.get("/", function(req, res) {
+   
+            res.render('index')
+        });
+        app.use("public", express.static("public"));
+        app.use('/views', express.static('views'));
 });
 
 
