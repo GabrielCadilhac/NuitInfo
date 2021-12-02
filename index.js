36ab40e9b7aaee0229f93743d13b2b05ba4d4ee4
LePaludV <59608384+LePaludV@users.noreply.github.com>
2021-12-02 23:40:35 +0100

    envoie des fichier

--- index.js
+++ index.js
@@ -9,34 +9,15 @@ var server = http.createServer(app);
 
 app.set('view engine', 'pug');
 app.set('views', 'views/');
-
-
-
-var Dict=new Map();
-
-Dict.set(1,[{nom:"Test",date:"",icone:"",fav:false,note:0,type:"",path:""}])
-Dict.get(1).push({nom:"Test2",date:"",icone:"",fav:false,note:0,type:"",path:""})
-//console.log(Dict);
-
-
-server.listen(process.env.PORT, process.env.IP, function()
-{
-        console.log("Serveur démarré");
-        app.get("/", function(req, res) {
-   
-            res.render('index')
-        });
-        
-        app.use('/views', express.static('views'));
-});
-
+;
+var ListeFile=JSON.parse(fs.readFileSync('Liste.json'))
 const Folder = './fichier/';
 
-parcourDossier(Folder)
+
 function parcourDossier(Folder){ 
 fs.readdir(Folder, (err, files) => {
   files.forEach(file => {
-    console.log(file);
+    //console.log(file);
     var stat = (fs.statSync(Folder+file));
     
     var size = stat.size
@@ -56,6 +37,35 @@ fs.readdir(Folder, (err, files) => {
     }
     else{
         //ajout du fichier
-        console.log(name,size,date,fav,note,type,path);
+        ListeFile.push({"nom":name,"size":size,"date":date,"fav":fav,"note":note,"type":type,"path":path})
+        //console.log(name,size,date,fav,note,type,path);
+        //console.log(ListeFile);
+        var data=JSON.stringify(ListeFile)
+        fs.writeFileSync('Liste.json', data)
+
     }
-  })})}
+    
+  })})
+
+}
+
+  //parcourDossier(Folder)
+
+  //console.log('Liste -> ' ,ListeFile);
+
+  
+
+server.listen(80, function()
+{
+        
+        console.log("Serveur démarré");
+        //console.log('TEST ->',ListeFile);
+        app.get("/", function(req, res) {
+            console.log('Chargement !!');
+            
+            res.render('index',{ListeFile:ListeFile})
+        });
+        
+        app.use('/views', express.static('views'));
+});
+
