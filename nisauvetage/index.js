84dabdc48fcda15277fc8623edcbc32b494ccd7d
yoshibox <meddacisohan@gmail.com>
2021-12-02 21:17:11 +0100

    docker

--- nisauvetage/index.js
+++ nisauvetage/index.js
@@ -1,11 +1,29 @@
 const http = require("http");
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+
+const url_db = "mongodb://mongodb:27017";
 
 var app = express();
 var server = http.createServer(app);
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/testbdd", function(req, res) {
+    MongoClient.connect(url_db, function(err, db)
+        {
+                var dbo = db.db("sauveteurs");
+                dbo.collection("test").find({}).toArray(function(err, result)
+                {
+                        res.render("livre", {"result": result, "test": "test"});
+                        db.close();
+                });
+        });
+
+    res.send("ui");
+    res.end();
 });
 
 app.use("public", express.static("public"));
