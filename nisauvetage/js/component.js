6587040a4e06890b99408488a2a289771bfbc01e
404-42 <joakim.botti81@gmail.com>
2021-12-02 23:27:30 +0100

    bonjour

---
+++ nisauvetage/js/component.js
@@ -0,0 +1,19 @@
+Vue.component("header-menu", {
+  props: [""],
+  template: `
+    <header>
+
+    </header>`
+});
+
+Vue.component("nav-menu", {
+  props: [""],
+  template: `<nav></nav>`
+});
+
+Vue.component("footer-menu", {
+  props: [""],
+  template: `
+    <footer></footer>
+    `
+});
