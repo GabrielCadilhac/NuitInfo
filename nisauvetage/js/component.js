15a5167202d2c93a9df68b8298c1836d80a19b9e
404-42 <joakim.botti81@gmail.com>
2021-12-03 04:49:34 +0100

    ;

--- nisauvetage/js/component.js
+++ nisauvetage/js/component.js
@@ -51,3 +51,23 @@ Vue.component("footer-menu", {
     </footer>
   `
 });
+
+
+Vue.component("sauveteurs-menu", {
+  props: ["answer"],
+  template: `
+    <h1>Quel dunkerquois n’a jamais lu les plaques commémoratives  apposées sur notre Leughenaer?</h1><br>
+
+    <p>Sur cette page on trouvera les sauveteurs décorés de  la Légion  d’Honneur et ou du Mérite Maritime pour services rendus.</p><br>
+    <p>La deuxième plaque apposée par les soins de la Société Humaine de Dunkerque rend hommage aux trois pilotes décédés dans le naufrage des TROIS SOEURS. La deuxième partie est un hommage à Jean-François Tixier sauveteur émérite des premières années de la société et qui perdra la vie en voulant sauver son prochain.</p><br>
+    <p>La station de Gravelines a aussi des sauveteurs qui ont été remerciés par l’attribution de la Légion d’Honneur.</p><br>
+    <p>Les légionnaires et titulaires du Mérite Maritime ont souvent été patron ou sous-patron des canots de sauvetage, patron de remorqueur, etc.</p><br>
+    <p>Ces fonctions et les défraiements qui leur sont liés ont été définis dès la création de la SCSN. Extrait du réglement ici .</p><br>
+
+    <h2>Membres fondateurs de la Société Humaine de Dunkerque</h2><br>
+
+    <div id="pierre_tombal">
+        <p v-for="i in answer">{{ i.nom }}</p>
+    </div>
+  `
+});
