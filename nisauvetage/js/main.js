ca49adfb7cfc6cf969e2cb201c1e79f2ce1dcff8
404-42 <joakim.botti81@gmail.com>
2021-12-03 06:38:30 +0100

    k:;

--- nisauvetage/js/main.js
+++ nisauvetage/js/main.js
@@ -13,5 +13,6 @@ document.querySelector("input").addEventListener("keyup", e => {
   if (e.keyCode == 13) {
     console.log(document.querySelector("input").value);
     app.get_data("/search", document.querySelector("input").value);
+    app.onglet = "Find";
   }
 });
