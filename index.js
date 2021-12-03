25647b279b7e065c231e73df43899a193be2ed96
LePaludV <59608384+LePaludV@users.noreply.github.com>
2021-12-03 06:54:24 +0100

    Fin nuit de l'info

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const http = require("http");
-const express = require("express");
+const http = require('http');
+const express = require('express');
 
 const pug=require('pug');
 var file = require('file-system');
@@ -27,8 +27,24 @@ fs.readdir(Folder, (err, files) => {
     var note= 0
     var s=file.split('.')
     var type= null
+    var icon='app'
     if(s[1]){
         type=s[1]
+       console.log(type);
+       console.log(type.indexOf(['jpg','pgn','gif','jpeg','svg']));
+        if (['jpg','png','gif','jpeg','svg'].indexOf(type)!=-1){
+          icon='image'
+        }
+        else if (['mp3','aac','wav','ogg','wma','ldac','flac'].indexOf(type)!=-1){
+          icon='son'
+        }
+        else if (['avi','mkv','mp4','flv','divx','mov','flv'].indexOf(type)!=-1){
+          icon='film'
+        }
+        else if (['txt','odt','doc','docx'].indexOf(type)!=-1){
+          icon='file-earmark-text'
+        }
+        
     }
     var name = s[0]
     var path=Folder+file
@@ -38,7 +54,7 @@ fs.readdir(Folder, (err, files) => {
     }
     else{
         //ajout du fichier
-        ListeFile.push({"nom":name,"size":size,"date":date,"fav":fav,"note":note,"type":type,"path":path,"nbAcces":nbAcces})
+        ListeFile.push({'nom':name,'size':size,'date':date,'fav':fav,'icon':icon,'note':note,'type':type,'path':path,'nbAcces':nbAcces})
         //console.log(name,size,date,fav,note,type,path);
         //console.log(ListeFile);
         var data=JSON.stringify(ListeFile)
@@ -54,44 +70,64 @@ if(ListeFile.length==0){
   parcourDossier(Folder);
 };
 
-console.log(ListeFile.length);
+console.log(ListeFile);
 
 
 function TriDivise (ListeFille) {  
-
-//console.log("liste avant tir-> ",ListeFile);
+  L1=[]
+  L2=[]
+  L3=[]
+ /*for( i in ListeFile){
+    if (ListeFile[i].note=1){
+      var temp=ListeFile.splice(i,1)
+      console.log("Filte dans L1 de ->",temp);
+      L1.push(temp)
+    }
+    else if (ListeFile[i].note=2){
+      var temp=ListeFile.splice(i,1)
+      console.log("Filte dans L2 de ->",temp);
+      L2.push(temp)
+    }
+    else if (ListeFile[i].note=3){
+      var temp=ListeFile.splice(i,1)
+      L3.push(temp)
+    }
+  }
+*/
+//console.log('liste avant tir-> ',ListeFile);
 ListeFile.sort(function (a, b) {
   return a.nbAcces - b.nbAcces;
 });
 var data=JSON.stringify(ListeFile)
 fs.writeFileSync('Liste.json', data)
-//console.log("liste apres tir-> ",ListeFile);
+//console.log('liste apres tir-> ',ListeFile);
 var L1=ListeFile.slice(0,ListeFile.length/3);
 var L2=ListeFile.slice(ListeFile.length/3,(ListeFile.length/3)*2);
 var L3=ListeFile.slice((ListeFile.length/3)*2);
 
-console.log("Liste L1 -> ",L1.length);
-console.log("Liste L2 -> ",L2.length);
-console.log("Liste L3 -> ",L3.length);
 
-var Data={"L1":L1,"L2":L2,"L3":L3}
+console.log('Liste L1 -> ',L1.length);
+console.log('Liste L2 -> ',L2.length);
+console.log('Liste L3 -> ',L3.length);
+
+var Data={'L1':L1,'L2':L2,'L3':L3}
 return Data
 }
 var data=JSON.stringify(ListeFile)
 fs.writeFileSync('Liste.json', data)
 
 //process.env.PORT, process.env.IP,
-server.listen(process.env.PORT, process.env.IP, function()
+server.listen(80, function()
 {
         
-        console.log("Serveur démarré");
+        console.log('Serveur démarré');
         //console.log('TEST ->',ListeFile);
-        app.get("/", function(req, res) {
+        app.get('/', function(req, res) {
             console.log('Chargement !!');
             
             res.render('index',{Data:TriDivise (ListeFile)})
         });
-        app.get("/click", function(req, res) {
+        app.get('/click', function(req, res) {
             console.log('modif !!');
             var nom=req.query.nom;
             console.log(nom);
@@ -104,8 +140,21 @@ server.listen(process.env.PORT, process.env.IP, function()
                 console.log('done');
               }
             }
-            res.render("index",{Data : TriDivise(ListeFile)})
+            res.render('index',{Data : TriDivise(ListeFile)})
         });
+
+        app.get('/setEnsemble', function(req, res) {
+          var nom = req.query.nom;
+          var ensemble=req.query.ensemble;
+          for(var i in ListeFile){
+              
+            if (ListeFile[i].nom==nom){
+              ListeFile[i].note=ensemble
+                console.log('done');
+            }
+          }
+          res.render('index',{Data : TriDivise(ListeFile)})
+        })
         
         app.use('/views', express.static('views'));
         app.use('/fichier', express.static('fichier'));
--- views/script.js
+++ views/script.js
@@ -23,15 +23,57 @@ $(function () {
 
     $(".fichier").draggable({appendTo: 'body', // Append to the body.
     zIndex: 1,
-    containment: $('document'),
-    helper: 'clone'})
+    containment: $('body'),
+    helper: 'clone'
+ 
+})
 
-    $( "moyen " ).droppable({
+    $( ".moyen " ).droppable({  
         drop: function( event, ui ) {
+            console.log(ui);
             console.log(this);
           $( this )
-            .addClass( "moyen" )
-            
+            .addClass( ".moyen" )
+            var nom=$($(ui.draggable).children()[1]).html();
+            var droppableId = $(this).attr("id");
+            SetEnsemble(nom,2)
         }
       });
+      $( ".grand " ).droppable({  
+        drop: function( event, ui ) {
+            console.log(ui);
+            console.log(this);
+          $( this )
+            .addClass( ".moyen" )
+            var nom=$($(ui.draggable).children()[1]).html();
+            
+            SetEnsemble(nom,3)
+        }
+      });
+
+      $( ".grand " ).droppable({  
+        drop: function( event, ui ) {
+            console.log(ui);
+            console.log(this);
+          $( this )
+            .addClass( ".moyen" )
+            var nom=$($(ui.draggable).children()[1]).html();
+            
+            SetEnsemble(nom,1)
+        }
+      });
+
+      function SetEnsemble(nom,ensemble){
+          $.ajax({
+              type: "get",
+              url: "/setEnsemble",
+              data: {nom:nom,
+                ensemble:ensemble
+                    },
+              dataType: "text",
+              success: function (res) {
+                $('body').html(res);
+              }
+          });
+      }
 });
